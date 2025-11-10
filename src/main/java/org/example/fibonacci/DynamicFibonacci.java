8a9b6929c574bd99d167cd40fc2d7e651a95232e
Anton Tsarenko <itantontsarenko@gmail.com>
2025-11-10 20:13:03 +0200

    Added calculations of complexities

--- src/main/java/org/example/fibonacci/DynamicFibonacci.java
+++ src/main/java/org/example/fibonacci/DynamicFibonacci.java
@@ -6,22 +6,27 @@ import java.util.Map;
 /**
  * Цей клас імплементує варіант алгоритму обчислення використовуючи
  * динамічне програмування.
- *
- *
+ * Оскільки на відміну від простої рекурсії, кожне значення Фібоначчі
+ * розраховується один раз, а складність кожного з методів containsKey, get,
+ * put є O(1), то часова складність - O(n).
+ * На відміну від простої рекурсії, де для підрахування просторової складності
+ * береться до уваги лише стек викликів методу, глибина якого n, то в цьому випадку
+ * є ще memo, який міститиме n елементів, тобто загальна просторова складність -
+ * O(n)(стек викликів) + О(n)(розмір мемо) = O(n)
  */
 public class DynamicFibonacci {
-    private final Map<Integer, Integer> cache = new HashMap<>();
+    private final Map<Integer, Integer> memo = new HashMap<>();
 
     public int calculate(int n) {
         if (n <= 1) {
             return n;
         }
-        if (cache.containsKey(n)) {
-            return cache.get(n);
+        if (memo.containsKey(n)) {
+            return memo.get(n);
         }
         int value = calculate(n - 1) + calculate(n - 2);
 
-        cache.put(n, value);
+        memo.put(n, value);
 
         return value;
     }
